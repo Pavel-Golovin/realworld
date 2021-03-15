4f6ecb7edd86f3f369bd784af468de1067290953
Pavel <Golovin.Pavel@outlook.com>
2021-03-15 03:07:28 +0300

    Рефакторинг

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,13 @@
 import React from 'react';
 
 const App = () => (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
+  <div className="App">
+    <header className="App-header">
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+    </header>
+  </div>
 );
-
-/*const foo = () => {
-  console.log("haha")
-}
-
-foo()*/
 
 export default App;
